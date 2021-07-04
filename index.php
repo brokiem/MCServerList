c694c926e85ced9430c6ee9ab781686bb0585871
brokiem <milyhqvideo@gmail.com>
2021-07-04 18:54:24 +0800

    custom font quicksand

--- index.php
+++ index.php
@@ -6,8 +6,6 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <link href="https://fonts.googleapis.com/css?family=Quicksand:300,500" rel="stylesheet">
-
     <link rel="icon" href="favicon.ico" type="image/x-icon">
 
     <link rel="stylesheet" href="src/css/styles.css">
