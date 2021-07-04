ad5b4f6d0be32b506c6d4b4645c93424358bd194
brokiem <milyhqvideo@gmail.com>
2021-07-04 14:51:05 +0800

    fixes

--- src/db/database.php
+++ src/db/database.php
@@ -1,11 +1,11 @@
 <?php
 
-$address = getenv("mysql-address");
-$db = getenv("mysql-db");
-$username = getenv("mysql-user");
-$password = getenv("mysql-pass");
+$mysql_address = getenv("mysql-address");
+$mysql_db = getenv("mysql-db");
+$mysql_username = getenv("mysql-user");
+$mysql_password = getenv("mysql-pass");
 
-$connection = new PDO("mysql:host=$address; dbname=$db", $username, $password);
+$connection = new PDO("mysql:host=$mysql_address; dbname=$mysql_db", $mysql_username, $mysql_password);
 $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 $connection->exec("
