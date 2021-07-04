de6df368673543bc2e5da24173e889f171ae9c40
brokiem <milyhqvideo@gmail.com>
2021-07-04 16:39:04 +0800

    fixes

--- index.php
+++ index.php
@@ -24,28 +24,25 @@
 <div class="container">
     <?php
 
-    include("src/db/database.php");
-    include("src/query/query.php");
+    include("src/cachemanager.php");
 
-    startQuery();
     saveCachedFile();
+    startQuery();
 
     $serversFile = @file_get_contents("cache/servers.json");
 
-    if (!$serversFile) {
-        $servers = json_decode($serversFile, true);
+    $servers = json_decode($serversFile, true);
 
-        foreach ($servers as $id => $data) {
-            // Card image
-            echo '<svg class="bd-placeholder-img card-img-top" width="100%" height="40" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" role="img"><img src="https://cdn.discordapp.com/attachments/833621011097845830/861064226637086750/crop.png" class="img-fluid rounded""></svg>';
-            // Card
-            echo '<div class="shadow p-3 mb-5 card mb-3 card-bg-dark"> 
+    foreach ($servers as $id => $data) {
+        // Card image
+        $status = $data["status"] === "offline" ? '<span class="badge badge-danger">Offline</span>' : '<span class="badge badge-success">Online</span>';
+        echo '<svg class="bd-placeholder-img card-img-top" width="100%" height="40" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" role="img"><img src="https://cdn.discordapp.com/attachments/833621011097845830/861064226637086750/crop.png" class="img-fluid rounded""></svg>';
+        echo '<div class="shadow p-3 mb-5 card mb-3 card-bg-dark"> 
                 <div class="card-body card-bg-dark rounded">
                     <h3 class="card-title">' . $data["title"] . '</h3>
-                    <h5><span class="badge bg-secondary">' . $data["address"] . ':' . $data["port"] . '</span> ' . $data["status"] . ' <span class="badge bg-primary">' . $data["version"] . '</span> <span class="badge bg-info">' . $data["players"] . '/' . $data["maxPlayers"] . '</span></h5>
+                    <h5><span class="badge bg-secondary">' . $data["address"] . ':' . $data["port"] . '</span> ' . $status . ' <span class="badge bg-primary">' . $data["version"] . '</span> <span class="badge bg-info">' . $data["players"] . '/' . $data["maxPlayers"] . '</span></h5>
                     <p class="card-text">' . $data["caption"] . '</p>
-                </div></div>';
-        }
+             </div></div>';
     }
     ?>
 </div>
---
+++ src/cachemanager.php
@@ -0,0 +1,131 @@
+<?php
+
+/** @noinspection MkdirRaceConditionInspection */
+function checkLastCached(): bool {
+    if (!is_dir("cache")) {
+        mkdir("cache");
+    }
+
+    $file = @file_get_contents("cache/lastExec.json");
+
+    if (!$file) {
+        file_put_contents("cache/lastExec.json", json_encode(["lastMysqlCache" => microtime(true)]));
+        return true;
+    }
+
+    if (!isset(json_decode($file, true)["lastMysqlCache"])) {
+        file_put_contents("cache/lastExec.json", json_encode(array_merge(json_decode($file, true), ["lastMysqlCache" => microtime(true)])));
+        return true;
+    }
+
+    if ((120.0 + (float)json_decode($file, true)["lastMysqlCache"]) < microtime(true)) {
+        file_put_contents("cache/lastExec.json", json_encode(array_merge(json_decode($file, true), ["lastMysqlCache" => microtime(true)])));
+        return true;
+    }
+
+    return false;
+}
+
+function saveCachedFile(bool $force = false): void {
+    if (!$force && !checkLastCached()) {
+        return;
+    }
+
+    include("db/database.php");
+
+    $list = $connection->query("SELECT * FROM serverlist");
+    $list->setFetchMode(PDO::FETCH_ASSOC);
+
+    $servers = [];
+
+    while (($row = $list->fetch(PDO::FETCH_ASSOC)) !== false) {
+        $query = $connection->query("SELECT * FROM querydata");
+        $query->setFetchMode(PDO::FETCH_ASSOC);
+
+        $servers[$row["id"]] = [
+            "title" => $row["title"],
+            "caption" => $row["caption"],
+            "address" => $row["address"],
+            "port" => $row["port"],
+        ];
+
+        while (($rowQ = $query->fetch(PDO::FETCH_ASSOC)) !== false) {
+            $servers[$row["id"]]["status"] = $rowQ["status"];
+            $servers[$row["id"]]["players"] = $rowQ["players"];
+            $servers[$row["id"]]["maxPlayers"] = $rowQ["maxplayers"];
+            $servers[$row["id"]]["version"] = $rowQ["version"];
+        }
+    }
+
+    file_put_contents("cache/servers.json", json_encode($servers));
+}
+
+/** @noinspection MkdirRaceConditionInspection */
+function checkLastQuery(): bool {
+    if (!is_dir("cache")) {
+        mkdir("cache");
+    }
+
+    $file = @file_get_contents("cache/lastExec.json");
+
+    if (!$file) {
+        file_put_contents("cache/lastExec.json", json_encode(["lastQuery" => microtime(true)]));
+        return true;
+    }
+
+    if (!isset(json_decode($file, true)["lastQuery"])) {
+        file_put_contents("cache/lastExec.json", json_encode(array_merge(json_decode($file, true), ["lastQuery" => microtime(true)])));
+        return true;
+    }
+
+    if ((120.0 + (float)json_decode($file, true)["lastQuery"]) < microtime(true)) {
+        file_put_contents("cache/lastExec.json", json_encode(array_merge(json_decode($file, true), ["lastQuery" => microtime(true)])));
+        return true;
+    }
+
+    return false;
+}
+
+function startQuery(bool $force = false) {
+    if (!$force && !checkLastQuery()) {
+        return;
+    }
+
+    include("src/db/database.php");
+    include("src/query/query.php");
+
+    $list = $connection->query("SELECT * FROM serverlist");
+    $list->setFetchMode(PDO::FETCH_ASSOC);
+
+    $queryRes = [];
+
+    while (($row = $list->fetch(PDO::FETCH_ASSOC)) !== false) {
+        $id = $row["id"];
+        $address = $row["address"];
+        $port = $row["port"];
+
+        $query = query($address, $port);
+
+        if ($query !== null) {
+            $queryRes[$id] = [
+                "id" => $id,
+                "status" => "online",
+                "players" => $query["Players"],
+                "maxplayers" => $query["MaxPlayers"],
+                "version" => $query["Version"],
+                "hostname" => $query["HostName"]
+            ];
+        }
+    }
+
+    foreach ($queryRes as $array) {
+        $status = $array["status"] ?? "offline";
+
+        if ($status !== "offline") {
+            $connection->prepare("INSERT INTO querydata (id, status, players, maxplayers, version, hostname) 
+                    VALUES (:id, :status, :players, :maxplayers, :version, :hostname)
+                    ON DUPLICATE KEY UPDATE id=VALUES(id), status=VALUES(status), players=VALUES(players), version=VALUES(version), hostname=VALUES(hostname)"
+            )->execute($array);
+        }
+    }
+}
