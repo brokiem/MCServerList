8ef71120845329430d29d01cddd84cb362ea1574
brokiem <milyhqvideo@gmail.com>
2021-07-04 22:07:53 +0800

    idooott

--- query.php
+++ query.php
@@ -5,12 +5,6 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <div class="text-center">
-        <br><br><br>
-        <h1 class="display-3">Loading...</h1>
-        <p class="lead">Please wait..</p><br><br>
-    </div>
-
     <script type="text/javascript">
         setTimeout(function () {
             window.location.href = "index";
@@ -23,4 +17,17 @@
     startQuery();
     ?>
 </head>
+
+<body>
+<?php include("src/navbar.php"); ?>
+<div id="home"></div>
+
+<div class="container">
+    <div class="text-center">
+        <br><br><br>
+        <h1 class="display-3">Loading...</h1>
+        <p class="lead">Please wait..</p><br><br>
+    </div>
+</div>
+</body>
 </html>
