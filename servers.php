12570becc84bb2b8551db38c79554f924786e6ff
brokiem <milyhqvideo@gmail.com>
2021-07-05 13:21:52 +0800

    infinite scroll

--- servers.php
+++ servers.php
@@ -14,6 +14,42 @@
     <link rel="stylesheet" href="src/css/styles.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+
+    <script>
+        $.ajax({
+            type: 'GET',
+            url: 'src/servers.php',
+            data: {
+                'offset': $('.servers').length,
+                'total': 5
+            },
+            success: function (data) {
+                if (false !== data) {
+                    $('#servers').append(data);
+                }
+            }
+        });
+
+        window.onscroll = function () {
+            if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+                $.ajax({
+                    type: 'GET',
+                    url: 'src/servers.php',
+                    data: {
+                        'offset': $('.servers').length,
+                        'total': 5
+                    },
+                    success: function (data) {
+                        if (false !== data) {
+                            $('#servers').append(data);
+                        }
+                    }
+                });
+            }
+        };
+    </script>
 </head>
 
 <body>
@@ -22,32 +58,20 @@
 <div id="home"></div>
 
 <div class="container">
+    <div class="servers" id="servers">
+
+    </div>
+
     <?php
-
     include("src/cachemanager.php");
-
     startQuery();
     saveCachedFile();
-
-    $serversFile = @file_get_contents("cache/servers.json");
-
-    if (is_string($serversFile)) {
-        $servers = json_decode($serversFile, true);
-        shuffle($servers);
-
-        foreach ($servers as $id => $data) {
-            if (isset($data["players"])) {
-                $status = $data["status"] === "offline" ? '<span class="badge badge-danger">Offline</span>' : '<span class="badge badge-success">Online</span>';
-                echo '<svg class="bd-placeholder-img card-img-top" width="100%" height="40" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" role="img"><img src="https://cdn.discordapp.com/attachments/833621011097845830/861064226637086750/crop.png" draggable="false" onmousedown="return false" style="user-drag: none" class="img-fluid rounded""></svg>';
-                echo '<div class="shadow p-3 mb-5 card mb-3 card-bg-dark"> 
-                <div class="card-body card-bg-dark rounded">
-                    <h3 class="card-title">' . $data["title"] . '</h3>
-                    <h5><span class="badge bg-secondary">' . $data["address"] . ':' . $data["port"] . '</span> ' . $status . ' <span class="badge bg-primary">' . $data["version"] . '</span> <span class="badge bg-info">' . $data["players"] . '/' . $data["maxPlayers"] . '</span></h5>
-                    <p class="card-text">' . $data["caption"] . '</p>
-                </div></div>';
-            }
-        }
-    }
     ?>
 </div>
+
+<footer class="card-bg-dark text-center text-lg-start">
+    <div class="text-center p-3">
+        © 2021 Copyright <b>MC Server List</b>
+    </div>
+</footer>
 </body>
---
+++ src/servers.php
@@ -0,0 +1,27 @@
+<?php
+
+include("db/database.php");
+
+$offset = $_GET['offset'] ?? 5;
+$total = $_GET['total'] ?? 10;
+
+$list = $connection->query("SELECT * FROM serverlist LIMIT $offset, $total");
+$list->setFetchMode(PDO::FETCH_ASSOC);
+
+while (($row = $list->fetch(PDO::FETCH_ASSOC)) !== false) {
+    $query = $connection->query("SELECT * FROM querydata");
+    $query->setFetchMode(PDO::FETCH_ASSOC);
+
+    while (($rowQ = $query->fetch(PDO::FETCH_ASSOC)) !== false) {
+        if (($row["id"] === $rowQ["id"]) && isset($rowQ["players"])) {
+            $status = $rowQ["status"] === "offline" ? '<span class="badge badge-danger">Offline</span>' : '<span class="badge badge-success">Online</span>';
+            echo '<div class="servers"> <svg class="bd-placeholder-img card-img-top" width="100%" height="40" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" role="img"><img src="https://cdn.discordapp.com/attachments/833621011097845830/861064226637086750/crop.png" draggable="false" onmousedown="return false" style="user-drag: none" class="img-fluid rounded""></svg>';
+            echo '<div class="shadow p-3 mb-5 card mb-3 card-bg-dark"> 
+            <div class="card-body card-bg-dark rounded">
+                <h3 class="card-title">' . $row["title"] . '</h3>
+                <h5><span class="badge bg-secondary">' . $row["address"] . ':' . $row["port"] . '</span> ' . $status . ' <span class="badge bg-primary">' . $rowQ["version"] . '</span> <span class="badge bg-info">' . $rowQ["players"] . '/' . $rowQ["maxplayers"] . '</span></h5>
+                <p class="card-text">' . $row["caption"] . '</p>
+            </div></div></div>';
+        }
+    }
+}
