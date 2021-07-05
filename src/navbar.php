f74e3f3ee2e371b9edadf5b064b83acb92294d21
brokiem <milyhqvideo@gmail.com>
2021-07-05 15:31:45 +0800

    clean up

--- src/navbar.php
+++ src/navbar.php
@@ -1,5 +1,5 @@
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
-      integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+      integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
 <nav class="navbar navbar-expand-lg navbar-dark navbar-bg-dark">
     <a class="navbar-brand" href="/"><b>MC Servers</b> Beta</a>
