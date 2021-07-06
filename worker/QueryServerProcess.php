45a95e9895de40745a405bd8484dff450c1d63ad
brokiem <milyhqvideo@gmail.com>
2021-07-06 12:13:24 +0800

    try this

--- worker/QueryServerProcess.php
+++ worker/QueryServerProcess.php
@@ -3,10 +3,10 @@
 start(init());
 
 function init(): PDO {
-    $mysql_address = getenv("mysql-address");
-    $mysql_db = getenv("mysql-db");
-    $mysql_username = getenv("mysql-user");
-    $mysql_password = getenv("mysql-pass");
+    $mysql_address = getenv("mysql-address-query");
+    $mysql_db = getenv("mysql-db-query");
+    $mysql_username = getenv("mysql-user-query");
+    $mysql_password = getenv("mysql-pass-query");
 
     $connection = new PDO("mysql:host=$mysql_address; dbname=$mysql_db", $mysql_username, $mysql_password);
     $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
