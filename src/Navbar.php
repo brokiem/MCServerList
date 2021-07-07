f8833401bd008cbb51b173935a26bace0f63f9e9
brokiem <milyhqvideo@gmail.com>
2021-07-07 13:14:57 +0800

    navbar text animation

--- src/Navbar.php
+++ src/Navbar.php
@@ -4,10 +4,8 @@
                 class="navbar-toggler-icon"></span></button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
-            <li class="nav-item"><a class="nav-link" href="/">Home</a>
-            </li>
-            <li class="nav-item"><a class="nav-link" href="/server/add">Add Server</a>
-            </li>
+            <li class="nav-item"><a class="nav-link effect-underline" href="/">Home</a></li>
+            <li class="nav-item"><a class="nav-link effect-underline" href="/server/add">Add Server</a></li>
         </ul>
     </div>
 </nav>
