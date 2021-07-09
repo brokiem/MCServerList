973515e9afe40171f851b4d8dff5265131d6f78e
brokiem <milyhqvideo@gmail.com>
2021-07-09 11:02:50 +0800

    remove ads

--- server/list.php
+++ server/list.php
@@ -56,8 +56,6 @@
             }, 700);
         });
     </script>
-    <script data-ad-client="ca-pub-1738139518784794" async
-            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 </head>
 
 <body>
