05484f23ed19ca1000fbfedfc9fa410eaa2428c7
brokiem <milyhqvideo@gmail.com>
2021-07-09 14:16:12 +0800

    zoom in animation

--- src/Navbar.php
+++ src/Navbar.php
@@ -1,4 +1,5 @@
-<nav class="navbar navbar-expand-lg navbar-dark navbar-bg-dark"><a class="navbar-brand" href="/"><b>MC Servers</b> Beta</a>
+<nav class="navbar navbar-expand-lg navbar-dark navbar-bg-dark"><a class="navbar-brand zoom-in" href="/"><b>MC
+            Servers</b> Beta</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span
                 class="navbar-toggler-icon"></span></button>
